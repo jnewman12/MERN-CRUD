90497c7c2c64803aa18e07a27a2bf7bb32119458
James Newman <jamesmnewman1@gmail.com>
2019-07-08 14:56:18 -0700

    Added to navbar

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     return(
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
+          <Link to={'/'} className="navbar-brand">React Hacker News</Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
             <li className="nav-item">
